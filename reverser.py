64bb8524d76b79cd9b1157e7f98fdbb9784da03d
Ruben Vandamme <github-com-00ff86@vandamme.email>
2019-07-20 18:39:14 +0200

    Initial checksum reverse engineering.

---
+++ reverser.py
@@ -0,0 +1,32 @@
+packets=["555300980501022F0119C4", "5553009805010211020CEE", "555300980501024400E1E8", "555300980501023e00F0DF"] # Sensor
+
+packets+=["554D00960301020017", "554D00960300010019",
+        "554D00970301020016", "554D00970300010018"] # Niveau 0 out?
+packets+=["554D009603010219FE", "554D00960300011BFE",
+        "554D00970300011BFD", "554D009703010219FD"] # Niveau 1 out
+packets+=["554D00970301022CEA", "554D00960300012FEA",
+        "554D00960301022CEB", "554D00970300012FE9"] # Niveau 2 out
+packets+=["554D00960301025DBA", "554D009603000163B6",
+        "554D009703000163B5", "554D00970301025DB9"] # Niveau 4 out
+packets+=["554D00960301023CDB", "554D00960300014ECB",
+        "554D00970301023CDA", "554D00970300014ECA"] # Niveau 3 out
+
+def function1(data, offset):
+    acc = offset
+    for x in data:
+        acc-=x
+    acc%=0xff
+    return acc
+
+for offset in range(0, 0x100):
+    for packet in packets:
+        assert(len(packet)%2==0)
+        #print(packet)
+        msb=packet[::2]
+        lsb=packet[1::2]
+        packetbytes=[int(''.join(x),16) for x in zip(msb, lsb)]
+
+        packetdata=packetbytes[0:-1]
+        crc=packetbytes[-1]
+        if crc == function1(packetdata, offset):
+            print("✔️ ", ' '.join([hex(i) for i in packetbytes]), hex(offset))
