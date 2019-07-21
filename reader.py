0ec4f86f78b4b954dd61495090242c06f206c9ce
Ruben Vandamme <github-com-00ff86@vandamme.email>
2019-07-21 13:52:13 +0200

    Improve reader to parse all current data in packets.

--- reader.py
+++ reader.py
@@ -1,15 +1,15 @@
 from sys import argv
-import serial
 import struct
+import serial
 
 assert argv[1], "Please provide a serial device e.g. /dev/ttyUSB0"
 SER = serial.Serial(argv[1], 2400, parity=serial.PARITY_NONE, timeout=1)
 def search_length():
-    print('y')
-    readbytes=SER.read(4)
+    print('z')
+    readbytes=SER.read(3)
     #print(readbytes)
     #readints = int.from_bytes(readbytes, "little")
-    readints = struct.unpack("<BBBB", readbytes)
+    readints = struct.unpack("<BBB", readbytes)
     print(readints)
     size = readints[-1]+1
     readbytes=SER.read(size)
@@ -18,21 +18,30 @@ def search_length():
     print(readints)
     return search_preamble
 
+def search_preamble2():
+    print('y')
+    readbyte = SER.read()
+    readdata = struct.unpack("<B", readbyte)[0]
+    print(readdata)
+    if readdata == 0x4d:
+        return search_length
+    if readdata == 0x00:
+        return search_length
+    if readdata == 0x53:
+        return search_length
+    if readdata == 0x55:
+        return search_preamble2
+    return search_preamble
+
 def search_preamble():
     print('x')
     readbyte = SER.read()
-    print(readbyte)
-    if readbyte == b'\x55':
-        return search_length
+    readdata = struct.unpack("<B", readbyte)[0]
+    print(readdata)
+    if readdata == 0x55:
+        return search_preamble2
     return search_preamble
 
 STATE = search_preamble
 while True:
     STATE = STATE()
-
-    #readbyte = SER.read()
-    #print(readbyte)
-    #x = something
-    #if readbyte == b'U':
-    #    print(SER.read(5))
-    #    x()
