50e82bf3a1250c8cc342d333579c60fe51cc04bf
Ruben Vandamme <github-com-00ff86@vandamme.email>
2019-09-07 10:55:12 +0200

    Fix warnings by syntastic.

--- comfospot40/state.py
+++ comfospot40/state.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from pdb import set_trace as bp
 
 class Zone:
     def __init__(self):
@@ -13,35 +12,33 @@ class Zone:
         self.timer = None
 
     def __placetimer(self):
-        if self.timer == None:
+        if self.timer is None:
             return "   0s"
         return "{:4}s".format(int((datetime.now()-self.timer).total_seconds()))
 
     def __placeintake(self):
-        if self.isintake == True:
+        if self.isintake:
             return "🏠⬅️"
-        elif self.isintake == False:
+        if not self.isintake:
             return "🏠➡️"
-        else:
-            return "__"
+        return "__"
 
     def __placeholder(self, var, num):
         if not var:
             return '_'*num
-        else:
-            return var
+        return var
 
     def __repr__(self):
         return self.__str__()
     def __str__(self):
         return "{}{} ({})🌡️ {}C, {}% ♻️  {}C, {}%".format(
-                self.__placeintake(),
-                self.__placetimer(),
-                self.__placeholder(self.fan_speed, 2),
-                self.__placeholder(self.inside_temperature,4),
-                self.__placeholder(self.inside_humidity,2),
-                self.__placeholder(self.recycled_temperature, 4),
-                self.__placeholder(self.recycled_humidity, 2))
+            self.__placeintake(),
+            self.__placetimer(),
+            self.__placeholder(self.fan_speed, 2),
+            self.__placeholder(self.inside_temperature, 4),
+            self.__placeholder(self.inside_humidity, 2),
+            self.__placeholder(self.recycled_temperature, 4),
+            self.__placeholder(self.recycled_humidity, 2))
 
 class State:
     def __init__(self):
@@ -52,11 +49,11 @@ class State:
             zone = self.zones.get(packet.getzone(), Zone())
             #print(packet.direction())
             if packet.extract():
-                zone.inside_humidity=packet.humidity()
-                zone.inside_temperature=packet.temperature()
+                zone.inside_humidity = packet.humidity()
+                zone.inside_temperature = packet.temperature()
             else:
-                zone.recycled_humidity=packet.humidity()
-                zone.recycled_temperature=packet.temperature()
+                zone.recycled_humidity = packet.humidity()
+                zone.recycled_temperature = packet.temperature()
 
             self.zones[packet.getzone()] = zone
 
@@ -69,17 +66,16 @@ class State:
                     zone.timer = datetime.now()
             #print(packet.direction())
             if packet.direction() == 1:
-                zone.fan_speed=packet.speed()
+                zone.fan_speed = packet.speed()
             self.zones[packet.getzone()] = zone
 
-    def __tozonestr(self,zone):
+    def __tozonestr(self, zone):
         if zone > 3 or zone < 1:
             return "Zone " + str(zone)
-        else:
-            return ('⓿','❶','❷','❸','❹','❺','❻','❼','❽','❾','❿')[zone]
+        return ('⓿', '❶', '❷', '❸', '❹', '❺', '❻', '❼', '❽', '❾', '❿')[zone]
 
     def __str__(self):
         keys = list(self.zones.keys())
         keys.sort()
-        zonestr=[self.__tozonestr(k) +": " + str(self.zones[k]) for k in keys]
-        return "; ".join(zonestr);
+        zonestr = [self.__tozonestr(k) + ": " + str(self.zones[k]) for k in keys]
+        return "; ".join(zonestr)
