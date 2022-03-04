8e8c88c4fdb129d79fe1274c7228a5f657c68c3b
Ruben Vandamme <github-com-00ff86@vandamme.email>
2022-03-04 15:30:46 +0100

    Initial publish to MQTT

--- server.py
+++ server.py
@@ -3,17 +3,25 @@ import sys
 import serial
 import serial_asyncio
 import comfospot40
+from asyncio_mqtt import Client
 
 
-async def main(devicename):
+async def main(devicename, mqtturi):
     reader, _ = await serial_asyncio.open_serial_connection(
         url=sys.argv[1], baudrate=2400, parity=serial.PARITY_NONE
     )
-    parser = comfospot40.Parser(reader)
-    while True:
-        print(await parser.run())
+    async with Client(mqtturi) as client:
+        parser = comfospot40.Parser(reader)
+        x = None
+        while True:
+            state = await parser.run()
+            print(state)
+            for zoneid, zonestate in state.zones.items():
+                x = client.publish("comfospot40/zones/zone{}/state".format(zoneid), payload=str(zonestate).encode(), qos=1)
+                asyncio.create_task(x)
 
 
 if __name__ == "__main__":
     assert sys.argv[1], "Please provide a serial device e.g. /dev/ttyUSB0"
-    asyncio.run(main(sys.argv[1]))
+    assert sys.argv[2], "Please provide a MQTT server"
+    asyncio.run(main(sys.argv[1], sys.argv[2]))
