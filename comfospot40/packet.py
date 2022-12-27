aec460aa932979a57558fde1c46653886b20423f
Ruben Vandamme <github-com-00ff86@vandamme.email>
2022-12-27 10:17:35 +0100

    Re-add zero compensation in the checksum.
    Restrict it to fan speed packets.

--- comfospot40/packet.py
+++ comfospot40/packet.py
@@ -12,13 +12,8 @@ class Packet:
             # Use the simplest theory based on the index.
             # It's currently unclear why other zeros don't adjust the checksum.
             if x == 0:
-                pass
-                # if i==5:
-                #    acc-=1
-                # if i==7:
-                #    acc-=1
-                # if i==8:
-                #    acc+=1
+                if i == 7 and data[3] in (0x97, 0x96):
+                    acc -= 1
             else:
                 acc -= x
         acc %= 0xFF
