964798984902535cb88252a9e7a90a6419e4b117
Ruben Vandamme <github-com-00ff86@vandamme.email>
2022-12-27 20:45:38 +0100

    Add poc tool to set the fan speed

---
+++ writer.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+from sys import argv
+from time import sleep
+import serial
+
+assert argv[1], "Please provide a serial device e.g. /dev/ttyUSB0"
+ser = serial.Serial(argv[1], 2400, parity=serial.PARITY_NONE, timeout=1)
+
+from comfospot40.create_packet import create_speed_packet
+
+intake = argv[2] == "in"
+
+value = 0
+if argv[3][-1] == "h":
+    value = int(argv[3][0:-1], 16)
+else:
+    value = int(argv[3])
+
+print(value)
+
+alt = False
+if len(argv) == 5:
+    print("alt")
+    alt = True
+
+packet = create_speed_packet(1, intake, int(value), alt)
+print(packet)
+print(" ".join([hex(i) for i in packet]))
+while True:
+    ser.write(packet)
+    print("writing")
+    # print(com.get_state())
+    sleep(1)
