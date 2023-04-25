7beaa3b797a92cb03dc83fa462af09455112f44a
Ruben Vandamme <github-com-00ff86@vandamme.email>
2023-04-25 08:22:17 +0000

    Add fan direction support

--- comfospot40/fanspeed.py
+++ comfospot40/fanspeed.py
@@ -4,7 +4,8 @@ from .value import Value
 class Fanspeed(Value):
     _oscillation = True
     _on = True
-    _mode = b"out"
+    _mode = b"min"
+    _direction_forward = True
 
     def set_fan_speed(self, temp):
         print("set fan", self._value)
@@ -24,11 +25,16 @@ class Fanspeed(Value):
             ),
             (self.topic_on_state, str(self._on).lower().encode()),
             (self.topic_mode_state, self._mode),
+            (self.topic_direction_state, "forward" if self._direction_forward else "reverse" ),
         )
 
     def set_oscillation(self, temp):
         print("Set oscillation ", temp, temp == b"true")
         self._oscillation = temp == b"true"
+
+    def set_direction(self, temp):
+        print("Set direction ", temp, temp == b"true")
+        self._direction_forward = temp == b"forward"
 
     def set_on(self, temp):
         print("Set on ", temp, temp == b"true")
@@ -40,6 +46,7 @@ class Fanspeed(Value):
 
     def do_subscribes(self):
         return (
+            (self.topic_direction_set, lambda x: self.set_direction(x)),
             (self.topic_oscillation_set, lambda x: self.set_oscillation(x)),
             (self.topic_on_state, lambda x: self.set_on(x)),
             (self.topic_mode_state, lambda x: self.set_mode(x)),
@@ -53,18 +60,22 @@ class Fanspeed(Value):
         self.topic_mode_state = self.prefix + "/preset/preset_mode_state"
         self.topic_oscillation_state = self.prefix + "/oscillation/state"
         self.topic_oscillation_set = self.prefix + "/oscillation/set"
+        self.topic_direction_state = self.prefix + "/direction/state"
+        self.topic_direction_set = self.prefix + "/direction/set"
         return {
             "name": "Comfospot40 Zone {0} Fan".format(zoneid),
             "~": self.prefix,
             "state_topic": self.topic_on_state,
             "command_topic": "~/on/set",
+            "direction_state_topic": self.topic_direction_state,
+            "direction_command_topic": self.topic_direction_set,
             "oscillation_state_topic": self.topic_oscillation_state,
             "oscillation_command_topic": self.topic_oscillation_set,
             "percentage_state_topic": self.topic_percentage_state,
             "percentage_command_topic": "~/speed/percentage",
             "preset_mode_state_topic": self.topic_mode_state,
             "preset_mode_command_topic": "~/preset/preset_mode",
-            "preset_modes": ["in", "out", "in low", "low", "mid", "high", "max"],
+            "preset_modes": ["low", "mid", "high", "max"],
             "qos": 0,
             "payload_on": "true",
             "payload_off": "false",
