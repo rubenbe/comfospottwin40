8ba314588e0f71a67866a39398c089bd0790bcee
Ruben Vandamme <github-com-00ff86@vandamme.email>
2024-05-24 13:47:20 +0200

    Pylint: fix most hal warnings

--- comfospot40/hal.py
+++ comfospot40/hal.py
@@ -1,9 +1,9 @@
-from asyncio import sleep
-from serial_asyncio import open_serial_connection
-import serial
 import json
 import time
 import logging
+from asyncio import sleep
+import serial
+from serial_asyncio import open_serial_connection
 from comfospot40 import State, Parser, create_packet
 
 
@@ -12,7 +12,9 @@ class Hal:
         self._state = state
         self._oscillation_time = oscillation_time
         self._oscillation_switch = time.monotonic()
+        self._reader = None
         self._writer = None
+        self.parser = None
 
     async def setup(self, devpath: str):
         self._reader, self._writer = await open_serial_connection(
@@ -34,7 +36,8 @@ class Hal:
             packet = create_packet.create_speed_packet(
                 zoneid, zonestate.fan_speed.direction_forward(), fan_speed, 0, True
             )
-            self._writer.write(bytes(packet)) if self._writer else None
+            if self._writer:
+                self._writer.write(bytes(packet))
             await sleep(0.1)
             counter = zonestate.counter_fan.get_fan_data(zonestate.fan_speed)
             # print(counter["direction"], counter["speed"])
@@ -42,7 +45,8 @@ class Hal:
                 zoneid, counter["direction"], counter["speed"], 1, True
             )
             # print("Writing")
-            self._writer.write(bytes(packet)) if self._writer else None
+            if self._writer:
+                self._writer.write(bytes(packet))
             await sleep(0.1)
 
     def storeState(self, storefile, state):
@@ -52,7 +56,7 @@ class Hal:
         try:
             loadedjson = json.load(storefile)
         except json.decoder.JSONDecodeError as e:
-            logging.info("Failed to decode json {}".format(e))
+            logging.info("Failed to decode json %s", e)
             return
         logging.info(loadedjson)
         state.from_json(loadedjson)
